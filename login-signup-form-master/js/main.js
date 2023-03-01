71100e2f4d872d1b5a7248c2b8f897456773d566
mohammedalqarra <mohammedalqarra2915@gmail.com>
2023-03-01 21:42:45 +0200

    init commit

---
+++ login-signup-form-master/js/main.js
@@ -0,0 +1,112 @@
+function setFormMessage(formElement, type, message) {
+  const messageElement = formElement.querySelector(".form__message");
+
+  messageElement.textContent = message;
+  messageElement.classList.remove(
+    "form__message--success",
+    "form__message--error"
+  );
+  messageElement.classList.add(`form__message--${type}`);
+}
+
+function setInputError(inputElement, message) {
+  inputElement.classList.add("form__input--error");
+  inputElement.parentElement.querySelector(
+    ".form__input-error-message"
+  ).textContent = message;
+}
+
+function clearInputError(inputElement) {
+  inputElement.classList.remove("form__input--error");
+  inputElement.parentElement.querySelector(
+    ".form__input-error-message"
+  ).textContent = "";
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  const loginForm = document.querySelector("#login");
+  const createAccountForm = document.querySelector("#createAccount");
+
+  document
+    .querySelector("#linkCreateAccount")
+    .addEventListener("click", (e) => {
+      e.preventDefault();
+      loginForm.classList.add("form--hidden");
+      createAccountForm.classList.remove("form--hidden");
+    });
+
+  document.querySelector("#linkLogin").addEventListener("click", (e) => {
+    e.preventDefault();
+    loginForm.classList.remove("form--hidden");
+    createAccountForm.classList.add("form--hidden");
+  });
+
+  loginForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    // Perform your AJAX/Fetch login
+
+    setFormMessage(loginForm, "error", "Invalid username/password combination");
+  });
+
+  document.querySelectorAll(".form__input").forEach((inputElement) => {
+    inputElement.addEventListener("blur", (e) => {
+      if (
+        e.target.id === "signupUsername" &&
+        e.target.value.length > 0 &&
+        e.target.value.length < 10
+      ) {
+        setInputError(
+          inputElement,
+          "Username must be at least 10 characters in length"
+        );
+      }
+    });
+
+    inputElement.addEventListener("input", (e) => {
+      clearInputError(inputElement);
+    });
+  });
+});
+
+/* connect*/
+let arrow = document.querySelectorAll(".arrow");
+for (var i = 0; i < arrow.length; i++) {
+  arrow[i].addEventListener("click", (e) => {
+    let arrowParent = e.target.parentElement.parentElement; //selecting main parent of arrow
+    console.log(arrowParent);
+    arrowParent.classList.toggle("showMenu");
+  });
+}
+let sidebar = document.querySelector(".sidebar");
+let sidebarBtn = document.querySelector(".bx-menu");
+console.log(sidebarBtn);
+sidebarBtn.addEventListener("click", () => {
+  sidebar.classList.toggle("close");
+});
+let HTMLBox = document.getElementById("HTMLBox");
+let HTMLButton = document.getElementById("HTMLButton");
+
+HTMLButton.onclick = function () {
+  HTMLBox.select();
+  document.execCommand("copy");
+  HTMLButton.innerText = "Codes Copied";
+};
+
+let wrapper = document.querySelector(".wrapper"),
+  selectBtn = wrapper.querySelector(".select-btn");
+let search = document.querySelector(".search");
+
+selectBtn.addEventListener("click", () => {
+  wrapper.classList.toggle("active");
+});
+
+function insertValue() {
+  let select = document.getElementById("select"),
+    txtVal = document.getElementById("val").value,
+    newOption = document.createElement("OPTION"),
+    newOptionVal = document.createTextNode(txtVal);
+
+  newOption.appendChild(newOptionVal);
+  select.insertBefore(newOption, select.lastChild);
+}
